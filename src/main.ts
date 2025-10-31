3a69dca0fa41bff0b12673735533e068a74fce60
Eyob Simachew <eyob.ugr-1326-16@aau.edu.et>
2025-10-31 04:41:19 +0300

    Adding_documentation

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',
+      'https://primetradeaibackend-production.up.railway.app/',
+    ],
     credentials: true,
   });
   app.use(cookieParser());
